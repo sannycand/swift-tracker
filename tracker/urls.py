9103581c4deddf73d09bb040b042976200973da6
sannycandidier <sannycandidier@gmail.com>
2017-08-15 18:28:23 +0800

    Initial commit

---
+++ tracker/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    url(r'^', TemplateView.as_view(template_name="base.html"))
+]
