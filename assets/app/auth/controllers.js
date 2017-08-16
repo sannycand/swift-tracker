109ccc9fb94757aade35f43bd3b7527c9e7ecdcb
sannycandidier <sannycandidier@gmail.com>
2017-08-16 19:25:39 +0800

    add signup form

--- assets/app/auth/controllers.js
+++ assets/app/auth/controllers.js
@@ -4,12 +4,17 @@
   angular
     .module('tracker')
     .controller('DashboardController', DashboardController)
+    .controller('AuhtAsideController', AuhtAsideController)
   ;
 
   function DashboardController ($scope) {
     var self = this;
   
   };
+
+  function AuhtAsideController ($scope) {
+    console.log('auth AuhtAsideController')
+  }
 
 
 })();
--- assets/app/controllers.js
+++ assets/app/controllers.js
@@ -4,6 +4,7 @@
   angular
     .module('tracker')
     .controller('IndexController', IndexController)
+    .controller('AsideController', AsideController)
   ;
 
   function IndexController ($scope) {
@@ -11,5 +12,21 @@
   
   };
 
+  function AsideController ($scope, UserService) {
+    var self = this;
+
+    self.signUp = signUp;
+
+    // signup form
+    function signUp (form) {
+      UserService.signup(form).then(function(resp) {
+        self.error_msg = '';
+        self.form = {}; // reset form
+      }).catch(function(error) {
+        self.error_msg = error.data;
+      });
+    };
+
+  };
 
 })();
