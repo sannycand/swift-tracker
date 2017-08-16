31c94f2b94f5a9e24022d71b61f08be188bfde00
sannycandidier <sannycandidier@gmail.com>
2017-08-16 12:56:33 +0800

    add project model

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Company(models.Model):
@@ -8,3 +9,13 @@ class Company(models.Model):
 
     def __str__(self):
         return '{}'.format(self.name)
+
+class Project(models.Model):
+    name = models.CharField(max_length=225)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    company = models.ForeignKey('Company')
+    is_archived = models.BooleanField(default=False)
+    date_created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return '{}-{}'.format(self.name, self.owner)
