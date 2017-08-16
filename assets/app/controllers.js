f3eba5b1f8b43548d29df3118e74286eff1f68f8
sannycandidier <sannycandidier@gmail.com>
2017-08-16 19:58:10 +0800

    add login form, view action for login form and signup form

--- assets/app/controllers.js
+++ assets/app/controllers.js
@@ -12,10 +12,15 @@
   
   };
 
-  function AsideController ($scope, UserService) {
+  function AsideController ($scope, $state, UserService) {
     var self = this;
 
+    self.currentView = 'login';
+    
     self.signUp = signUp;
+    self.login = login;
+    self.actionView = actionView;
+
 
     // signup form
     function signUp (form) {
@@ -25,6 +30,20 @@
       }).catch(function(error) {
         self.error_msg = error.data;
       });
+    };
+
+    // login form
+    function login (form) {
+      UserService.login(form).then(function(resp) {
+        window.location.reload();
+      }).catch(function(error){
+        self.error_msg = error.data;
+      });
+    };
+
+    // change view action
+    function actionView (action) {
+      self.currentView = action;
     };
 
   };
